b47803d5aff98221de5790487442da960881fef6
Ermek <ermekerlan06@gmail.com>
2025-02-24 11:22:37 +0600

    1commit

--- src/main/java/com/example/project/Hangman.java
+++ src/main/java/com/example/project/Hangman.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Hangman extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Hangman.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Hangman.class.getResource("hangman-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 668, 419);
         stage.setTitle("Hangman Game");
         stage.setScene(scene);
